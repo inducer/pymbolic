55b2ce28dcf36aed8fa9284bcb2ad947f500a3fb
Andreas Kloeckner <inform@tiker.net>
2008-07-27 21:44:05 +0200

    Mark pymbolic as non-zip-safe.

--- setup.py
+++ setup.py
@@ -52,5 +52,7 @@ setup(name="pymbolic",
       install_requires=[
           'pytools>=2'
           ],
-      package_dir={"pymbolic": "src", "pymbolic.mapper":"src/mapper"}
+      package_dir={"pymbolic": "src", "pymbolic.mapper":"src/mapper"},
+
+      zip_safe=False,
      )
