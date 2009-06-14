d1d9c01a62d407ba07ade0a200477584ef02f3f1
Andreas Kloeckner <inform@tiker.net>
2009-06-14 03:39:22 -0400

    Modify tests to make them suitable for py.test.

--- test/test_pymbolic.py
+++ test/test_pymbolic.py
@@ -1,21 +1,12 @@
-import unittest
+def test_expand():
+    from pymbolic import var, expand
 
+    x = var("x")
+    u = (x+1)**5
+    expand(u)
 
-
-
-class TestPymbolic(unittest.TestCase):
-    def test_expand(self):
-        from pymbolic import var, expand
-
-        x = var("x")
-        u = (x+1)**5
-        expand(u)
-
-    def test_substitute(self):
-        from pymbolic import parse, substitute, evaluate
-        u = parse("5+x.min**2")
-        xmin = parse("x.min")
-        assert evaluate(substitute(u, {xmin:25})) == 630
-            
-if __name__ == '__main__':
-    unittest.main()
+def test_substitute():
+    from pymbolic import parse, substitute, evaluate
+    u = parse("5+x.min**2")
+    xmin = parse("x.min")
+    assert evaluate(substitute(u, {xmin:25})) == 630
