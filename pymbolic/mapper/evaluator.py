b8a884598d2cc1499fe6dbd5c697c6ff7f649358
Andreas Kloeckner <inform@tiker.net>
2010-03-04 14:11:25 -0500

    Fix polynomial evaluation.

--- pymbolic/mapper/evaluator.py
+++ pymbolic/mapper/evaluator.py
@@ -47,17 +47,17 @@ class EvaluationMapper(RecursiveMapper):
         return self.rec(expr.base) ** self.rec(expr.exponent)
 
     def map_polynomial(self, expr):
-        import pymbolic
-
         # evaluate using Horner's scheme
         result = 0
         rev_data = expr.data[::-1]
+        ev_base = self.rec(expr.base)
+
         for i, (exp, coeff) in enumerate(rev_data):
             if i+1 < len(rev_data):
                 next_exp = rev_data[i+1][0]
             else:
                 next_exp = 0
-            result = (result+coeff)*expr.base**(exp-next_exp)
+            result = (result+coeff)*ev_base**(exp-next_exp)
 
         return result
 
