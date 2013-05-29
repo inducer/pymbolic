459444c4022ee842f67e108b8470444e61c85b39
Andreas Kloeckner <inform@tiker.net>
2013-05-29 10:54:29 -0400

    Add Gaussian elimination, affine equation solving.

--- pymbolic/algorithm.py
+++ pymbolic/algorithm.py
@@ -28,6 +28,8 @@ from pytools import memoize
 
 
 
+# {{{ integer powers
+
 def integer_power(x, n, one=1):
     """Compute :math:`x^n` using only multiplications.
 
@@ -49,24 +51,9 @@ def integer_power(x, n, one=1):
         x = x * x
         n //= 2
 
+# }}}
 
-
-def gcd(q, r):
-    return extended_euclidean(q, r)[0]
-
-def gcd_many(*args):
-    if len(args) == 0:
-        return 1
-    elif len(args) == 1:
-        return args[0]
-    else:
-        return reduce(gcd, args)
-
-def lcm(q, r):
-    return abs(q*r)//gcd(q, r)
-
-
-
+# {{{ euclidean algorithm
 
 def extended_euclidean(q, r):
     """Return a tuple *(p, a, b)* such that :math:`p = aq + br`,
@@ -96,8 +83,23 @@ def extended_euclidean(q, r):
     return q, Q[0], Q[1]
 
 
+def gcd(q, r):
+    return extended_euclidean(q, r)[0]
 
+def gcd_many(*args):
+    if len(args) == 0:
+        return 1
+    elif len(args) == 1:
+        return args[0]
+    else:
+        return reduce(gcd, args)
 
+def lcm(q, r):
+    return abs(q*r)//gcd(q, r)
+
+# }}}
+
+# {{{ fft
 
 @memoize
 def find_factors(N):
@@ -209,7 +211,7 @@ def sym_fft(x, sign=1):
     return NearZeroKiller()(
             fft(wrap_intermediate(x), sign=sign, wrap_intermediate=wrap_intermediate))
 
-
+# }}}
 
 
 
@@ -231,6 +233,156 @@ def csr_matrix_multiply(S, x):
 
 
 
+# {{{ gaussian elimination
+
+def gaussian_elimination(mat, rhs):
+    m, n = mat.shape
+    i = 0
+    j = 0
+
+    while i < m and j < n:
+        # {{{ find pivot in column j, starting in row i
+
+        nonz_row = None
+        for k in range(i, m):
+            if mat[k,j]:
+                nonz_row = k
+                break
+
+        # }}}
+
+        if nonz_row is not None:
+            # swap rows i and nonz
+            mat[i], mat[nonz_row] = \
+                    (mat[nonz_row].copy(), mat[i].copy())
+            rhs[i], rhs[nonz_row] = \
+                    (rhs[nonz_row].copy(), rhs[i].copy())
+
+            for u in range(0, m):
+                if u == i:
+                    continue
+                if not mat[u, j]:
+                    # already 0
+                    continue
+
+                l = lcm(mat[u, j], mat[i, j])
+                u_fac = l//mat[u, j]
+                i_fac = l//mat[i, j]
+
+                mat[u] = u_fac*mat[u] - i_fac*mat[i]
+                rhs[u] = u_fac*rhs[u] - i_fac*rhs[i]
+
+                assert mat[u, j] == 0
+
+            i += 1
+
+        j += 1
+
+    for i in range(m):
+        g = gcd_many(*(
+            [a for a in mat[i] if a]
+            +
+            [a for a in rhs[i] if a]))
+
+        mat[i] //= g
+        rhs[i] //= g
+
+    return mat, rhs
+
+# }}}
+
+# {{{ symbolic (linear) equation solving
+
+def solve_affine_equations_for(targets, equations):
+    """
+    :arg targets: A list of variable names for which to solve.
+    :arg equations: A list of tuples ``(lhs, rhs)``.
+    :return: a dict mapping target names to their values.
+    """
+    import numpy as np
+
+    from pymbolic.mapper.dependency import DependencyMapper
+    dep_map = DependencyMapper(composite_leaves=False)
+
+    # fix an order for targets
+    targets_set = set(targets)
+    target_idx_lut = dict((tgt_name, idx)
+            for idx, tgt_name in enumerate(targets))
+
+    def get_deps(expr):
+        return set(var.name for var in dep_map(expr))
+
+    # Find non-target variables, fix order for them
+    # Last non-target is constant.
+    nontargets = set()
+    for lhs, rhs in equations:
+        nontargets.update(get_deps(lhs) - targets_set)
+        nontargets.update(get_deps(rhs) - targets_set)
+    nontargets_list = list(nontargets)
+    nontarget_idx_lut = dict((var_name, idx)
+            for idx, var_name in enumerate(nontargets_list))
+
+    from pymbolic.mapper.coefficient import CoefficientCollector
+    coeff_coll = CoefficientCollector()
+
+    # {{{ build matrix and rhs
+
+    mat = np.zeros((len(equations), len(targets_set)), dtype=object)
+    rhs_mat = np.zeros((len(equations), len(nontargets)+1), dtype=object)
+
+    for i_eqn, (lhs, rhs) in enumerate(equations):
+        for lhs_factor, coeffs in [(1, coeff_coll(lhs)), (-1, coeff_coll(rhs))]:
+            for key, coeff in coeffs.iteritems():
+                if key in targets:
+                    mat[i_eqn, target_idx_lut[key]] = lhs_factor*coeff
+                elif key in nontargets:
+                    rhs_mat[i_eqn, nontarget_idx_lut[key]] = -lhs_factor*coeff
+                elif key == 1:
+                    rhs_mat[i_eqn, -1] = -lhs_factor*coeff
+                else:
+                    raise ValueError("key '%s' not understood" % key)
+
+    # }}}
+
+    mat, rhs_mat = gaussian_elimination(mat, rhs_mat)
+
+    # FIXME /!\ Does not check for overdetermined system.
+
+    from pymbolic import var
+
+    result = {}
+    for j, target in enumerate(targets):
+        nonz_row = np.where(mat[:, j])
+        if len(nonz_row) != 1:
+            raise RuntimeError("cannot uniquely solve for '%s'" % target)
+
+        (nonz_row,), = nonz_row
+
+        if abs(mat[nonz_row, j]) != 1:
+            raise RuntimeError("division with remainder in linear solve for '%s'"
+                    % target)
+        div = mat[nonz_row, j]
+
+        target_val = int(rhs_mat[nonz_row, -1]) // div
+        for nontarget, coeff in zip(nontargets_list, rhs_mat[nonz_row]):
+            target_val += (int(coeff) // div) * var(nontarget)
+
+        result[target] = target_val
+
+    if 0:
+        for lhs, rhs in equations:
+            print lhs, '=', rhs
+        print "-------------------"
+        for lhs, rhs in result.iteritems():
+            print lhs, '=', rhs
+
+    return result
+
+# }}}
+
+
+
+
 if __name__ == "__main__":
     import integer
     q = integer.Integer(14)
@@ -238,3 +390,5 @@ if __name__ == "__main__":
     gcd, a, b = extended_euclidean(q, r)
     print gcd, "=", a, "*", q, "+", b, "*", r
     print a*q + b*r
+
+# vim: foldmethod=marker
