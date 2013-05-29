84ada75e56491f7a8cba768b66a2b691b5f50aa8
Andreas Kloeckner <inform@tiker.net>
2013-05-29 10:53:40 -0400

    Add CoefficientCollector.

---
+++ pymbolic/mapper/coefficient.py
@@ -0,0 +1,81 @@
+from __future__ import division
+
+__copyright__ = "Copyright (C) 2013 Andreas Kloeckner"
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+
+
+from pymbolic.mapper import Mapper
+
+
+
+
+class CoefficientCollector(Mapper):
+    def map_sum(self, expr):
+        stride_dicts = [self.rec(ch) for ch in expr.children]
+
+        result = {}
+        for stride_dict in stride_dicts:
+            for var, stride in stride_dict.iteritems():
+                if var in result:
+                    result[var] += stride
+                else:
+                    result[var] = stride
+
+        return result
+
+    def map_product(self, expr):
+        result = {}
+
+        children_coeffs = [self.rec(child) for child in expr.children]
+
+        idx_of_child_with_vars = None
+        for i, child_coeffs in enumerate(children_coeffs):
+            for k in child_coeffs:
+                if isinstance(k, str):
+                    if (idx_of_child_with_vars is not None
+                            and idx_of_child_with_vars != i):
+                        raise RuntimeError(
+                                "nonlinear expression")
+                    idx_of_child_with_vars = i
+
+        other_coeffs = 1
+        for i, child_coeffs in enumerate(children_coeffs):
+            if i != idx_of_child_with_vars:
+                assert len(child_coeffs) == 1
+                other_coeffs *= child_coeffs[1]
+
+        if idx_of_child_with_vars is None:
+            return {1: other_coeffs}
+        else:
+            return dict(
+                    (var, other_coeffs*coeff)
+                    for var, coeff in
+                    children_coeffs[idx_of_child_with_vars].iteritems())
+
+        return result
+
+    def map_constant(self, expr):
+        return {1: expr}
+
+    def map_variable(self, expr):
+        return {expr.name: 1}
