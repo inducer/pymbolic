19e1f9afb7be4fd3a6012165164b64ba4b063b6d
Andreas Kloeckner <inform@tiker.net>
2014-02-19 15:02:19 -0600

    Move PersistentHashWalkMapper from loopy

---
+++ pymbolic/mapper/persistent_hash.py
@@ -0,0 +1,47 @@
+from __future__ import division
+
+__copyright__ = "Copyright (C) 2009-2013 Andreas Kloeckner"
+
+__license__ = """
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+
+from pymbolic.mapper import WalkMapper
+
+
+class PersistentHashWalkMapper(WalkMapper):
+    """A subclass of :class:`loopy.symbolic.WalkMapper` for constructing
+    persistent hash keys for use with
+    :class:`pytools.persistent_dict.PersistentDict`.
+
+    See also :meth:`LoopyKeyBuilder.update_for_pymbolic_expression`.
+    """
+
+    def __init__(self, key_hash):
+        self.key_hash = key_hash
+
+    def visit(self, expr):
+        self.key_hash.update(type(expr).__name__.encode("utf8"))
+
+    def map_variable(self, expr):
+        self.key_hash.update(expr.name.encode("utf8"))
+
+    def map_constant(self, expr):
+        self.key_hash.update(repr(expr).encode("utf8"))
