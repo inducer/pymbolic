82d57c96c930ce9ff9761833a2bc5877f18f8201
zachjweiner <zachjweiner@gmail.com>
2019-09-22 21:56:39 -0500

    appease flake8

--- pymbolic/mapper/differentiator.py
+++ pymbolic/mapper/differentiator.py
@@ -22,9 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-import math
-import cmath
-
 import pymbolic
 import pymbolic.primitives as primitives
 import pymbolic.mapper
