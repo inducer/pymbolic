39a0657a3a85aab3e10b62efd723dd8d372dd095
[6~ <inform@tiker.net>
2020-06-07 01:34:45 -0500

    Placate flake8

--- pymbolic/mapper/differentiator.py
+++ pymbolic/mapper/differentiator.py
@@ -82,7 +82,7 @@ class DifferentiationMapper(pymbolic.mapper.RecursiveMapper):
         >>> from pymbolic.mapper.differentiator import DifferentiationMapper as DM
         >>> print(DM(x)(expr))
         (((x + 5)**3 + x*3*(x + 5)**2)*(x + -1)**2 + (-1)*2*(x + -1)*x*(x + 5)**3) / (x + -1)**2**2
-    """
+    """  # noqa: E501
 
     def __init__(self, variable, func_map=map_math_functions_by_name,
                  allowed_nonsmoothness=None):
