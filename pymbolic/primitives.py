be615915010ca3b501f71591fd2d63e55d09a486
Matt Wala <wala1@illinois.edu>
2020-07-01 22:23:31 -0500

    Fix so that numpy isn't required

--- pymbolic/primitives.py
+++ pymbolic/primitives.py
@@ -1668,16 +1668,18 @@ def make_common_subexpression(field, prefix=None, scope=None):
             or field.scope == scope):
         # Don't re-wrap
         return field
-    
-    import numpy
-    
-    have_obj_array = (
-            isinstance(field, numpy.ndarray)
-            and field.dtype.char == "O")
 
     try:
-        logical_shape = field.shape
-    except AttributeError:
+        import numpy
+        have_obj_array = (
+            isinstance(field, numpy.ndarray)
+            and field.dtype.char == "O")
+        logical_shape = (
+            field.shape
+            if isinstance(field, numpy.ndarray)
+            else ())
+    except ImportError:
+        have_obj_array = False
         logical_shape = ()
 
     from pymbolic.geometric_algebra import MultiVector
