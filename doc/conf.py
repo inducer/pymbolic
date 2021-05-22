7955c8164aacbd0c25a30d2c5c7156b480e53737
Andreas Klöckner <inform@tiker.net>
2021-05-22 18:23:14 -0500

    Drop autodoc_typehints from sphinx config
    https://github.com/inducer/modepy/pull/38#issuecomment-846472970

--- doc/conf.py
+++ doc/conf.py
@@ -50,9 +50,6 @@ pygments_style = "sphinx"
 
 html_theme = "furo"
 
-html_theme_options = {}
-
-
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     "https://docs.python.org/3": None,
@@ -63,4 +60,3 @@ intersphinx_mapping = {
 }
 
 autoclass_content = "class"
-autodoc_typehints = "description"
